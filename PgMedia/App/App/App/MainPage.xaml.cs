bf16cab11d3d613136295a9f183e1eb28f5877fd
Gabriel Ribeiro Rossi <gabriel.note96@hotmail.com>
2020-09-27 22:59:07 -0300

    ok

---
+++ PgMedia/App/App/App/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Precisamos habilitar permissão de ler e escrever no manisfest do android
+            //Botão direito no projeto android -> Propriedades -> Android Manifest -> READ_EXTERNAL_STORAGE e WRITE_EXTERNAL_STORAGE
+
+            btnFoto.Clicked += TirarFoto();
+            private void async TirarFoto(object) { }
+        }
+
+    }
+}
